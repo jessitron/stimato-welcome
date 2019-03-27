1eeccf88da1034789c5992eea0a5cf080e2456c9
Jessica Kerr <jessitron@atomist.com>
2019-03-27 15:26:02 -0500

    Serve a static home page

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ' + err);
 });
-console.log("Hello, Stimato!");
+console.log("Hello, Stimato! 'sup?");
+
+const app = express();
+
+app.use(express.static("public"));
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
